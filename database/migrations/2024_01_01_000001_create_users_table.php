a80588c538dd3f3f38c468d9702eb3fcf0a658e8
stevenricky <silabanm91@gmail.com>
2025-11-19 09:10:54 +0700

    Update 2024_01_01_000001_create_users_table.php

--- database/migrations/2024_01_01_000001_create_users_table.php
+++ database/migrations/2024_01_01_000001_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes(); // ← TAMBAH INI
         });
     }
 
